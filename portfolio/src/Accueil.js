eead2f93f1b8f5271f20044191fd4255fb68678c
Nouna321 <zaitsadouneogmail.com>
2020-12-20 17:07:12 +0100

    init

---
+++ portfolio/src/Accueil.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Accueil extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div className='h-screen bg-header bg-center bg-cover'>
+                <div className='h-full bg-gray-900 bg-opacity-60'></div>
+            </div>
+        )
+    }
+}
+
+export default Accueil
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                header: "url('/src/img/christina-wocintechchat-com-PISwYqWIGPc-unsplash.jpg')",
+            }),
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
